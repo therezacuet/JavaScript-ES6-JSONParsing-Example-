1ccca6863ad1f1e853dea18f186e88efc153087e
Rezaul Islam <rejaulcuet88@gmail.com>
2018-07-18 17:41:59 +0600

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ myPromise.then((res) => {
 });
 */
 
-let photos;
+var sd;
 
 function getData(method, url){
   return new Promise(function(resolve, reject){
@@ -41,63 +41,34 @@ function getData(method, url){
   });
 }
 
-
-
-
-//var albumID = filterSelection.options[filterSelection.selectedIndex].value;
-
-//filterSelection.onclick = myClickHandler(filterSelection.value);
-
-function myClickHandler(value) {
-      alert(value);
-}
-
 function filteredData(){
 
-
-  var filterSelectionValue = document.getElementById('inputGroupSelect01').value;
+  var filterSelectionValue = parseInt(document.getElementById('inputGroupSelect01').value);
  
   getData('GET', 'https://jsonplaceholder.typicode.com/photos').then(function(data){
 
-  
-
   let photos = JSON.parse(data);
+  sd.push(JSON.parse(data));
   let output = '';
-  let url = "https://jsonplaceholder.typicode.com/photos/";
 
   var jsonData = photos.filter((item) => {
-    return item.albumId === filterSelectionValue;
+
+    if (filterSelectionValue == 0) {
+      return item;
+    }else{
+      return item.albumId === filterSelectionValue;
+    }  
+    
   });
 
-  
-
-  //We do that to ensure to get a correct JSON
-  //var my_json = JSON.stringify(data)
-  //We can use {'name': 'Lenovo Thinkpad 41A429ff8'} as criteria too
-  //var filtered_json = find_in_object(JSON.parse(my_json), {albumId: albumID});
-
-  
-  if (filterSelectionValue == 0) {
-    for(let photo of photos){
-      output += `
-
-      <div class="col-lg-4 col-sm-6 text-center mb-4">
-            <img class="rounded-circle img-fluid d-block mx-auto" src=${photo.thumbnailUrl} alt="">
-            <h3><a href=${url+photo.id} >${photo.title}</a>
-              <small>Driver</small>
-            </h3>
-            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
-          </div> 
-      `;
-    }
-  }else{
+  //console.log(jsonData);
 
     for(let photo of jsonData){
       output += `
 
       <div class="col-lg-4 col-sm-6 text-center mb-4">
             <img class="rounded-circle img-fluid d-block mx-auto" src=${photo.thumbnailUrl} alt="">
-            <h3><a href=${url+photo.id} >${photo.title}</a>
+            <h3><a href="" onclick="details(id)" value=${photo.id}>${photo.title}</a>
               <small>Driver</small>
             </h3>
             <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
@@ -105,22 +76,15 @@ function filteredData(){
       `;
     }
 
-  }
-
-  console.log(filterSelectionValue);
-
-  console.log(jsonData);
-
-  console.log(photos);
-
-
-
   document.getElementById('template').innerHTML = output;
 
-}).catch(function(err){
-  console.log(err);
-});
+  }).catch(function(err){
+    console.log(err);
+  });
 
 }
+
+
+console.log("fkhgfk");
 
 
