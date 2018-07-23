c480f4002f328655cdba1412bbeb7594884c8b24
Rezaul Islam <rejaulcuet88@gmail.com>
2018-07-23 15:42:30 +0600

    Details Page added

--- main.js
+++ main.js
@@ -15,8 +15,6 @@ myPromise.then((res) => {
 });
 */
 
-var sd;
-
 function getData(method, url){
   return new Promise(function(resolve, reject){
     var xhr = new XMLHttpRequest();
@@ -48,7 +46,6 @@ function filteredData(){
   getData('GET', 'https://jsonplaceholder.typicode.com/photos').then(function(data){
 
   let photos = JSON.parse(data);
-  sd.push(JSON.parse(data));
   let output = '';
 
   var jsonData = photos.filter((item) => {
@@ -68,10 +65,10 @@ function filteredData(){
 
       <div class="col-lg-4 col-sm-6 text-center mb-4">
             <img class="rounded-circle img-fluid d-block mx-auto" src=${photo.thumbnailUrl} alt="">
-            <h3><a href="" onclick="details(id)" value=${photo.id}>${photo.title}</a>
+            <h3><a href="#details" onclick="details(this)" value=${photo.id}>${photo.title}</a>
               <small>Driver</small>
             </h3>
-            <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            <p>What does this team member to? Keep it shot. This is also a great spot for social links!</p>
           </div> 
       `;
     }
@@ -84,7 +81,45 @@ function filteredData(){
 
 }
 
+function details(a){
 
-console.log("fkhgfk");
+  document.getElementById('container').style.display ='none';
+  var id = a.getAttribute('value');
+  var url = "https://jsonplaceholder.typicode.com/photos/"+id;
 
+  getData('GET', url).then(function(data){
 
+    let details = JSON.parse(data);
+    let output = '';
+
+    console.log(details.title);
+
+        output = `
+        <h2 style="text-align:center">User Profile Card</h2>
+        <div class="card" >
+          <img src=${details.url} alt="John" style="width:100%;">
+          <h1>${details.title.substring(1,12)}</h1>
+          <p class="title">CEO & Founder, Example</p>
+          <p>Harvard University</p>
+          <div style="margin: 24px 0;">
+            <a href="#"><i class="fa fa-dribbble"></i></a> 
+            <a href="#"><i class="fa fa-twitter"></i></a>  
+            <a href="#"><i class="fa fa-linkedin"></i></a>  
+            <a href="#"><i class="fa fa-facebook"></i></a> 
+         </div>
+         <p><button>Contact</button></p>
+        </div>
+        `;
+
+    document.getElementById('output').innerHTML = output;
+
+  }).catch(function(err){
+    console.log(err);
+  });
+
+}
+
+var filterSelectionValue = parseInt(document.getElementById('inputGroupSelect01').value);
+if (filterSelectionValue == 0) {
+  filteredData();
+};
